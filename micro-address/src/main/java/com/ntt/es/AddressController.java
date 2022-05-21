a333ae4b982cb0f86daeed880ef7b5bc8aad493e
jj <jotajota1302@hotmail.com>
2022-05-21 12:00:17 +0200

    first commit

---
+++ micro-address/src/main/java/com/ntt/es/AddressController.java
@@ -0,0 +1,22 @@
+package com.ntt.es;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@RestController
+@RequestMapping("/address")
+@OpenAPIDefinition(info =@Info(title = "Address API", version = "1.0", description = "Documentation Address API v1.0"))
+public class AddressController {
+	
+	@GetMapping
+	public ResponseEntity<String> sayHello(){
+		
+		return ResponseEntity.ok("Hello");
+	}
+
+}
