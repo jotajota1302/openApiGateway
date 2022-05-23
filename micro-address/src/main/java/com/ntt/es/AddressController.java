5441c33d61cfcf9d5e5f549edfbccfd02c5938f4
jj <jotajota1302@hotmail.com>
2022-05-23 09:29:22 +0200

    commit

--- micro-address/src/main/java/com/ntt/es/AddressController.java
+++ micro-address/src/main/java/com/ntt/es/AddressController.java
@@ -1,6 +1,7 @@
 package com.ntt.es;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 @RestController
 @RequestMapping("/address")
 @OpenAPIDefinition(info =@Info(title = "Address API", version = "1.0", description = "Documentation Address API v1.0"))
+@CrossOrigin
 public class AddressController {
 	
 	@GetMapping
